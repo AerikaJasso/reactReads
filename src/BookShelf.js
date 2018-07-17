edb06f04159344b60ffc2a8fd59f18309e3f5642
Ærika <aerikajasso@gmail.com>
2018-07-16 21:10:01 -0700

    feat: Create event listener to CurrentReads Component.  - onChange event triggers changeStatus() passing 2 args:   1. Status String.   2. BookID.  - Invoke props.onHandleStatusChange() pass args 1 & 2 to    BookShelf (parent) Component.  - BookShelf Component handles state change, updates books    array.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -6,7 +6,33 @@ import Read from './Read';
 
 class BookShelf extends Component {
   state = {
-    books: []
+   books: [{
+    bookId: 1,
+    status: 'currentlyReading'
+   },
+   {
+    bookId: 2,
+    status: 'read'
+   },
+   {
+    bookId: 3,
+    status: 'wantToRead'
+   }
+  ]
+  }
+
+  handleStatusChange = (event, id) => {
+    const updatedBooksArray = this.state.books.map((book) => {
+      if (book.bookId === id){
+        book.status = event;
+      }
+      return book;
+    });
+    this.setState(() => {
+      return {
+        books: updatedBooksArray
+      }
+    });
   }
   render(){
     return(
@@ -18,7 +44,10 @@ class BookShelf extends Component {
             <div className="list-books-content">
               <div>
                 <CurrentReads
-                
+                  onHandleStatusChange={this.handleStatusChange}
+                  books={this.state.books.filter((currentBook) => (
+                    currentBook.status === 'currentlyReading'))
+                  }
                 />
                 <FutureReads 
 
